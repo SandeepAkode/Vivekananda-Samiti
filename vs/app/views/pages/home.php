8c022cd802c13bed81e2757c3a6759540c18b7fb
SandeepAkode <sandeep.akode@gmail.com>
2017-06-02 12:20:45 +0530

    Add files via upload

---
+++ vs/app/views/pages/home.php
@@ -0,0 +1,25 @@
+<?php include (APP_ROOT.'/app/views/base.php'); ?>
+
+<?php startblock('central_pane') ?>
+	<div style="font-size: 1.5em;">Upcoming Activities</div>
+	<h3><span style="font-size: 1.2em;"><a href="vylc_2014">"Vivekananda Youth Leadership Convention - 2014"</a></span></h3> 
+  
+	<p style="margin-top: 50px; border-top: 1px solid black;">
+		<span style="font-size: 1.2em;"><b>Vivekananda Samiti</b></span> is one of the oldest clubs under the Students' Gymkhana functioning for over 40 years, thus spreading the message of Swami Vivekananda among the campus residents. We have conducted several activities which include community service like literary camps and health camps for the under-privileged children in and around the campus, weekly discussions on Swami Vivekananda's life and teachings, meditation sessions, talks and workshops on value education, distribution of Vivekananda literature among youth. Since a large fraction of the student population is going through stress and various emotional and mental problems, Vivekananda Samiti is playing a significant role in motivating and inspiring the youth to perform their duties informed by human values.
+	</p>
+        
+
+
+<?php endblock() ?>
+
+<?php startblock('tail') ?>
+<style>
+	#central_pane > h3{
+		text-align: center;
+	}
+	
+	#central_pane > p{
+		margin-left: 0;
+	}
+</style>
+<?php endblock() ?>
